a6910d404357bffad25440e181ca97823a8b9d2c
Arthur Hermes <89710458+ArthurHermes@users.noreply.github.com>
2024-09-06 08:44:04 -0300

    a

--- trab_design_software/Atividade5/ImplementacaoJava/BancoItau.java
+++ trab_design_software/Atividade5/ImplementacaoJava/BancoItau.java
@@ -1,12 +1,12 @@
 public class BancoItau implements IBancoGeral{
     
     public ContaAbstrata AbrirConta(Cliente c, String NumeroConta, Double SaldoInicial, String tipo){
-
+        return ContaAbstrata;
     }
     public boolean depositar(ContaAbstrata c, Double valor){
-
+        return true;
     }
     public boolean sacar(ContaAbstrata c, Double valor){
-        
+        return true;
     }
 }
