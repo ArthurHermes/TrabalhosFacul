bc751d0c94e27331395ce3350aedd972d7a583c3
Ricardo Amaro <147277909+Refilx1@users.noreply.github.com>
2024-10-22 12:17:56 -0300

    HASH

--- Estrutura_de_Dados/TabelaHash/TabelaHashProbingLinear.java
+++ Estrutura_de_Dados/TabelaHash/TabelaHashProbingLinear.java
@@ -1,3 +1,10 @@
-public class TabelaHashProbingLinear {
-    
+public class TabelaHashProbingLinear extends TabelaHash {
+    public TabelaHashProbingLinear(int tamanho) {
+        super(tamanho);
+    }
+
+    @Override
+    protected int funcaoHash(String chave) {
+        return Math.abs(chave.hashCode() % tamanho); // Usando Math.abs para evitar índices negativos
+    }
 }
