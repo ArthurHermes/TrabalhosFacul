66237403fdd60137c39baee126a6193e1d2225c3
Ricardo Amaro <147277909+Refilx1@users.noreply.github.com>
2024-10-31 11:40:58 -0300

    Update teste.py

--- Verificacao_E_Validacao/teste_TDD/teste.py
+++ Verificacao_E_Validacao/teste_TDD/teste.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, main
 
 def calcular_area_retangulo(base, altura):
     pass
@@ -6,4 +6,11 @@ def calcular_area_retangulo(base, altura):
 
 class TestCalcularAreaRetangulo(TestCase):
     def test_area_retangulo(self):
-        self
+        self.assertEqual(calcular_area_retangulo(5, 10), 50)
+        self.assertEqual(calcular_area_retangulo(2, 3), 6)
+        self.assertEqual(calcular_area_retangulo(0, 10), 0)
+        self.assertEqual(calcular_area_retangulo(5, 0), 0)
+        self.assertEqual(calcular_area_retangulo(7.5, 4), 30)
+
+if __name__==    "__main__":
+    main()
