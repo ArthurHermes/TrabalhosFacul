566a00c94676b471a3fa40d37332c18d573ad455
Arthur Hermes <89710458+ArthurHermes@users.noreply.github.com>
2024-10-31 11:36:30 -0300

    Create teste.py

---
+++ Verificacao_E_Validacao/teste_TDD/teste.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+def calcular_area_retangulo(base, altura):
+    pass
+
+
+class TestCalcularAreaRetangulo(TestCase):
+    def test_area_retangulo(self):
+        self
