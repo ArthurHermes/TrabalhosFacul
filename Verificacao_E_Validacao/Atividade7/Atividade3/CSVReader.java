681936369481693502f40588d19942a8c66fd252
Ricardo Amaro <147277909+Refilx1@users.noreply.github.com>
2024-11-11 15:20:58 -0300

    att3

---
+++ Verificacao_E_Validacao/Atividade7/Atividade3/CSVReader.java
@@ -0,0 +1,27 @@
+package Verificacao_E_Validacao.Atividade7.Atividade3;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReader {
+    public List<String> readCSV(String filePath) {
+        List<String> names = new ArrayList<>();
+        String line;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            while ((line = br.readLine()) != null) {
+                names.add(line.trim()); // Adiciona o nome lido à lista
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+
+        if (names.isEmpty()) {
+            System.err.println("Nenhum nome foi lido do arquivo.");
+        }
+
+        return names;
+    }
+}
